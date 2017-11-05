b573689d0af6c83b9310b26f2dab99ba71d57155
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-11-05 01:23:05 -0500

    clases de configuracion

--- src/main/java/pe/com/sunshineandina/dto/ComisionTO.java
+++ src/main/java/pe/com/sunshineandina/dto/ComisionTO.java
@@ -9,17 +9,21 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  *
  * @author alonsorn
  */
+@Entity
+@Table(name="TSA_COMISION")
 public class ComisionTO implements Serializable {
 
     @Id
