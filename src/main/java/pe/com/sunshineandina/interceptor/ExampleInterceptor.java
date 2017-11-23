1ee974e4508ebbc50d0c6bf252828a5e2c9ec3ae
fernandozdv <fernandozdv@gmail.com>
2017-11-22 19:51:15 -0500

    fix interceptor

--- src/main/java/pe/com/sunshineandina/interceptor/ExampleInterceptor.java
+++ src/main/java/pe/com/sunshineandina/interceptor/ExampleInterceptor.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import pe.com.sunshineandina.dto.UsuarioTO;
 
 /**
  *
@@ -23,9 +24,11 @@ public class ExampleInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("aca estoy " + request.getRequestURI());
-        if (!request.getRequestURI().equals("/SistemaGestionNegocio/")) {
+        if (!request.getRequestURI().equals("/SistemaGestionNegocio/") &&
+                !request.getRequestURI().equals("/SistemaGestionNegocio/login")) {
             System.out.println("dsp del uri");
             session = request.getSession();
+            System.out.println((UsuarioTO)session.getAttribute("usuario"));
             if (session.getAttribute("usuario") == null) {
                 System.out.println("dsp del usuario");
                 response.sendRedirect("/SistemaGestionNegocio/");
